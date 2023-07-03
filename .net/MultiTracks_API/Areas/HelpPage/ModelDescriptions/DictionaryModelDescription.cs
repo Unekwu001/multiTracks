0c183f708d82aa2523ff31ddbb9bb089bbda0d36
Unekwu001 <tusedor360@gmail.com>
2023-07-03 18:30:17 +0100

    Quick fix

--- .net/MultiTracks_API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MultiTracks_API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- .net/MultiTracks_API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MultiTracks_API.WebApiApplication" Language="C#" %>
