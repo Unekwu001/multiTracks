041e1b15a46d2940bef321fccfa255f5926304cb
Unekwu001 <tusedor360@gmail.com>
2023-07-04 11:00:08 +0100

    implemented Add Artist API

--- .net/API/Program.cs
+++ .net/API/Program.cs
@@ -1,18 +1,23 @@
-using API.Repositories.Artist;
 using System.Configuration;
 using System.Text.Json.Serialization;
+using API.Repositories.Song;
+using API.Repositories.Artist.SearchArtist;
+using API.Repositories.Artist.AddArtist;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IArtistRepository, ArtistRepository>(provider => new ArtistRepository("Data source=localhost;DATABASE=MultiTracksDB;Integrated Security=True"));
 
- 
+// Each API needs to handle its own connectionString since scenerios may arise where Different Databases are involved.
+builder.Services.AddScoped<ISearchArtistRepository, SearchArtistRepository>(provider => new SearchArtistRepository("Data source=localhost;DATABASE=MultiTracksDB;Integrated Security=True"));
+builder.Services.AddScoped<ISongsRepository, SongsRepository>(provider => new SongsRepository("Data source=localhost;DATABASE=MultiTracksDB;Integrated Security=True"));
+builder.Services.AddScoped<IAddArtistRepository, AddArtistRepository>(provider => new AddArtistRepository("Data source=localhost;DATABASE=MultiTracksDB;Integrated Security=True"));
 
 var app = builder.Build();
 
--- .net/API/Repositories/Song/ISongsRepository.cs
+++ .net/API/Repositories/Song/ISongsRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace API.Repositories.Song
+﻿using System.Data;
+
+namespace API.Repositories.Song
 {
 	public interface ISongsRepository
 	{
+		int GetTotalSongCount();
+		string GetSongs(int pageNumber, int pageSize);
+		
 	}
 }
